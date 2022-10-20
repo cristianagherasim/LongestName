5e638315155daabe57f4a9d09a4dae66a2054b4f
Cristiana <cristiana.gherasim@gmail.com>
2022-10-20 19:02:53 -0400

    LongestNameLab

---
+++ longestname.js
@@ -0,0 +1,19 @@
+let name1 = 'Teo';
+let name2 = 'Cristiana';
+let name3 = "Evelyn";
+
+if((name1.length === name2.length) && (name1.length === name3.length)){
+    console.log('All three names, ' + name1 + ', ' + name2 + ', ' + name3 + ', are the same length');
+} else if (name1.length === name2.length && name2.length > name3.length){
+    console.log(name1 + ' and ' + name2 + ' tie for the longest name.');
+} else if (name1.length === name3.length && name3.length > name2.length){
+    console.log(name1 + ' and ' + name3 + ' tie for the longest name.');
+} else if (name2.length === name3.length && name3.length > name1.length){
+    console.log(name2 + ' and ' + name3 + ' tie for the longest name.');
+} else if ((name1.length > name2.length) && (name1.length > name3.length)){
+    console.log(name1 + ' has the longest name.')
+} else if ((name2.length > name1.length) && (name2.length > name3.length)){
+    console.log(name2 + ' has the longest name.')
+} else if ((name3.length > name1.length) && (name3.length > name2.length)){
+    console.log(name3 + ' has the longest name.')
+}
